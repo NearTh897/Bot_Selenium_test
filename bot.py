0f9c976263dfc28cc6d835841e4c413654a080aa
NearTh897 <theoargolo@gmail.com>
2026-07-25 20:06:25 -0300

    Refactor Firefox driver initialization and remove unnecessary comments

--- bot.py
+++ bot.py
@@ -5,13 +5,10 @@
 
 # Bibliotecas necessárias
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from time import sleep
 
-service = Service('/home/theo/Área de trabalho/Selenium/Bot_Selenium_test/geckodriver') # Caminho para o driver do Firefox (geckodriver)
-driver = webdriver.Firefox(service=service)
-
-driver.get("https://selenium.dev") # Acessa o site "https://selenium.dev"
+driver = webdriver.Firefox() 
+driver.get("https://selenium.dev")
 
 texto_da_pagina = driver.page_source # Obtém o código-fonte da página usando o string driver.page_source
 palavra_alvo = "Selenium" # Escolhe a palavra que deseja contar no código-fonte da página
@@ -23,12 +20,3 @@ print(f"A palavra '{palavra_alvo}' aparece {numero_de_ocorrencias} vezes nesta p
 sleep(5)  # Aguarda 5 segundos antes de fechar o navegador
 driver.quit()
 
-
-# Infelizmente, não é possível executar este código aqui, 
-# pois ele requer a instalação do Selenium e do driver do Firefox,
-# além de um ambiente gráfico para abrir o navegador. 
-# No entanto, você pode executar este código em seu próprio ambiente local, 
-# desde que tenha o Selenium e o driver do Firefox instalados corretamente.
-# O código requer o Selenium instalado e possui direitos e licença para uso.
-
-# Com Venv, ele dá erros dentro do sistema.
